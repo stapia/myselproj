06a36de542e67b7453afef3015a694c2bcb8a4c2
stapia <tapiasebastian@hotmail.com>
2021-05-02 19:07:59 -0400

    stapia | added 'shadowJar' gradle task to create 'fat jar'.  Added temporary entry point

---
+++ src/main/java/entrypoint/Main.java
@@ -0,0 +1,8 @@
+package entrypoint;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world!!!".concat(String.valueOf(args.length)));
+    }
+}
